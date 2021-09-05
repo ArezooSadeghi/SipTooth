39068c3ca6bf056c67853f77d65830fa3defe5ba
AREZOO <arezoo.sdghi@gmail.com>
2021-09-05 18:02:39 +0430

    g

--- app/src/main/java/com/example/toothtest/MainActivity.java
+++ app/src/main/java/com/example/toothtest/MainActivity.java
@@ -1,10 +1,15 @@
 package com.example.toothtest;
 
 import android.content.Intent;
+import android.graphics.PixelFormat;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.provider.Settings;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Toast;
@@ -14,9 +19,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
 import com.example.toothtest.databinding.ActivityMainBinding;
+import com.example.toothtest.databinding.FloatWindowBinding;
+import com.example.toothtest.databinding.ToothBinding;
+
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding binding;
+    private WindowManager windowManager;
+    private FloatWindowBinding floatWindowBinding;
+    private ToothBinding toothBinding;
+    private WindowManager.LayoutParams layoutParams;
+    private float lastX, lastY;
+    private static int TYPE;
+
     private static final int REQUEST_CODE = 0;
 
     @Override
@@ -25,11 +41,7 @@ public class MainActivity extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         super.onCreate(savedInstanceState);
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
-        if (checkDrawOverlayPermission()) {
-            startToothService();
-        } else {
-            getDrawOverlayPermission();
-        }
+        handleEvents();
     }
 
     @Override
@@ -39,7 +51,7 @@ public class MainActivity extends AppCompatActivity {
             case REQUEST_CODE:
                 if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
                     if (Settings.canDrawOverlays(this)) {
-                        startToothService();
+                        handleWindowManager();
                     } else {
                         Toast.makeText(MainActivity.this, "دسترسی داده نشد", Toast.LENGTH_SHORT).show();
                     }
@@ -51,19 +63,55 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        Intent starter = ToothService.start(this);
-        stopService(starter);
+        if (floatWindowBinding != null && floatWindowBinding.getRoot().isShown()) {
+            windowManager.removeView(floatWindowBinding.getRoot());
+        }
+
+        if (toothBinding != null && toothBinding.getRoot().isShown()) {
+            windowManager.removeView(toothBinding.getRoot());
+        }
     }
 
-    private boolean checkDrawOverlayPermission() {
-        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
-            return true;
-        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O_MR1) {
-            return Settings.canDrawOverlays(this);
-        } else {
-            if (Settings.canDrawOverlays(this)) {
+    private void handleEvents() {
+        binding.ivEnableAssistiveTouch.setOnTouchListener(new View.OnTouchListener() {
+            private float xCoordinate, yCoordinate;
+            private long startClickTime;
+            private static final int MAX_CLICK_DURATION = 200;
+
+            @Override
+            public boolean onTouch(View view, MotionEvent event) {
+                switch (event.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+                        startClickTime = Calendar.getInstance().getTimeInMillis();
+                        xCoordinate = view.getX() - event.getRawX();
+                        yCoordinate = view.getY() - event.getRawY();
+                        break;
+                    case MotionEvent.ACTION_UP:
+                        long clickDuration = Calendar.getInstance().getTimeInMillis() - startClickTime;
+                        if (clickDuration < MAX_CLICK_DURATION) {
+                            if (canDrawOverlay()) {
+                                handleWindowManager();
+                            } else {
+                                getDrawOverlayPermission();
+                            }
+                        }
+                        break;
+                    case MotionEvent.ACTION_MOVE:
+                        view.animate().x(event.getRawX() + xCoordinate).y(event.getRawY() + yCoordinate).setDuration(0).start();
+                        lastX = (int) (event.getRawX() + xCoordinate);
+                        lastY = (int) (event.getRawY() + yCoordinate);
+                        break;
+                }
                 return true;
             }
+        });
+    }
+
+    private boolean canDrawOverlay() {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
+            return true;
+        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            return Settings.canDrawOverlays(this);
         }
         return false;
     }
@@ -73,8 +121,126 @@ public class MainActivity extends AppCompatActivity {
         startActivityForResult(starter, REQUEST_CODE);
     }
 
-    private void startToothService() {
-        Intent starter = ToothService.start(this);
-        startService(starter);
+    private void handleWindowManager() {
+        binding.ivEnableAssistiveTouch.setVisibility(View.GONE);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            TYPE = WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY;
+        } else {
+            TYPE = WindowManager.LayoutParams.TYPE_PHONE;
+        }
+
+        floatWindowBinding = DataBindingUtil.inflate(
+                LayoutInflater.from(this),
+                R.layout.float_window,
+                null,
+                false);
+
+        toothBinding = DataBindingUtil.inflate(
+                LayoutInflater.from(this),
+                R.layout.tooth,
+                null,
+                false);
+
+        layoutParams = new WindowManager.LayoutParams(
+                ViewGroup.LayoutParams.WRAP_CONTENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT,
+                TYPE,
+                WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE,
+                PixelFormat.TRANSLUCENT);
+
+        layoutParams.x = (int) lastX;
+        layoutParams.y = (int) lastY;
+        /*        layoutParams.gravity = Gravity.TOP | Gravity.RIGHT;*/
+
+        windowManager = (WindowManager) getSystemService(WINDOW_SERVICE);
+
+
+        windowManager.addView(floatWindowBinding.getRoot(), layoutParams);
+
+        floatWindowBinding.getRoot().setOnTouchListener(new View.OnTouchListener() {
+            private float xCoordinate, yCoordinate;
+            private long startClickTime;
+            private static final int MAX_CLICK_DURATION = 200;
+
+            @Override
+            public boolean onTouch(View view, MotionEvent event) {
+                switch (event.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+                        startClickTime = Calendar.getInstance().getTimeInMillis();
+                        xCoordinate = view.getX() - event.getRawX();
+                        yCoordinate = view.getY() - event.getRawY();
+                        break;
+                    case MotionEvent.ACTION_UP:
+                        long clickDuration = Calendar.getInstance().getTimeInMillis() - startClickTime;
+                        if (clickDuration < MAX_CLICK_DURATION) {
+                            int[] locationClose = new int[2];
+                            floatWindowBinding.ivClose.getLocationOnScreen(locationClose);
+                            int[] locationTL = new int[2];
+                            floatWindowBinding.tvTl.getLocationOnScreen(locationTL);
+                            int[] locationCardView = new int[2];
+                            floatWindowBinding.cardView.getLocationOnScreen(locationCardView);
+                            if (!(event.getRawX() >= locationCardView[0]) || !(event.getRawX() <= locationCardView[0] + floatWindowBinding.cardView.getWidth()) || !(event.getRawY() >= locationCardView[1]) || !(event.getRawY() <= locationCardView[1] + floatWindowBinding.cardView.getHeight())) {
+                                floatWindowBinding.getRoot().setVisibility(View.GONE);
+                                binding.ivEnableAssistiveTouch.setVisibility(View.VISIBLE);
+                            } else if (event.getRawX() >= locationClose[0] && event.getRawX() <= locationClose[0] + floatWindowBinding.ivClose.getWidth() && event.getRawY() >= locationClose[1] && event.getRawY() <= locationClose[1] + floatWindowBinding.ivClose.getHeight()) {
+                                floatWindowBinding.getRoot().setVisibility(View.GONE);
+                                binding.ivEnableAssistiveTouch.setVisibility(View.VISIBLE);
+                            } else if (event.getRawX() >= locationTL[0] && event.getRawX() <= locationTL[0] + floatWindowBinding.tvTl.getWidth() && event.getRawY() >= locationTL[1] && event.getRawY() <= locationTL[1] + floatWindowBinding.tvTl.getHeight()) {
+                                floatWindowBinding.getRoot().setVisibility(View.GONE);
+                                if (toothBinding.getRoot().getVisibility() == View.GONE) {
+                                    toothBinding.getRoot().setVisibility(View.VISIBLE);
+                                } else {
+                                    windowManager.addView(toothBinding.getRoot(), layoutParams);
+                                }
+                            }
+                        }
+                        break;
+                    case MotionEvent.ACTION_MOVE:
+                        /* view.animate().x(event.getRawX() + xCoordinate).y(event.getRawY() + yCoordinate).setDuration(0).start();*/
+                        layoutParams.x = (int) (event.getRawX() + xCoordinate);
+                        layoutParams.y = (int) (event.getRawY() + yCoordinate);
+                        windowManager.updateViewLayout(floatWindowBinding.getRoot(), layoutParams);
+                        break;
+                }
+                return true;
+            }
+        });
+
+        toothBinding.getRoot().setOnTouchListener(new View.OnTouchListener() {
+            private float xCoordinate, yCoordinate;
+            private long startClickTime;
+            private static final int MAX_CLICK_DURATION = 200;
+
+            @Override
+            public boolean onTouch(View view, MotionEvent event) {
+                switch (event.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+                        startClickTime = Calendar.getInstance().getTimeInMillis();
+                        xCoordinate = view.getX() - event.getRawX();
+                        yCoordinate = view.getY() - event.getRawY();
+                        break;
+                    case MotionEvent.ACTION_UP:
+                        long clickDuration = Calendar.getInstance().getTimeInMillis() - startClickTime;
+                        if (clickDuration < MAX_CLICK_DURATION) {
+                            int[] locationClose = new int[2];
+                            toothBinding.ivClose.getLocationOnScreen(locationClose);
+                            int[] locationToothContainer = new int[2];
+                            toothBinding.toothContainer.getLocationOnScreen(locationToothContainer);
+                            if (!(event.getRawX() >= locationToothContainer[0]) || !(event.getRawX() <= locationToothContainer[0] + toothBinding.toothContainer.getWidth()) || !(event.getRawY() >= locationToothContainer[1]) || !(event.getRawY() <= locationToothContainer[1] + toothBinding.toothContainer.getHeight())) {
+                                toothBinding.getRoot().setVisibility(View.GONE);
+                                floatWindowBinding.getRoot().setVisibility(View.VISIBLE);
+                            } else if (event.getRawX() >= locationClose[0] && event.getRawX() <= locationClose[0] + toothBinding.ivClose.getWidth() && event.getRawY() >= locationClose[1] && event.getRawY() <= locationClose[1] + toothBinding.ivClose.getHeight()) {
+                                toothBinding.getRoot().setVisibility(View.GONE);
+                                floatWindowBinding.getRoot().setVisibility(View.VISIBLE);
+                            }
+                        }
+                        break;
+                    case MotionEvent.ACTION_MOVE:
+                        view.animate().x(event.getRawX() + xCoordinate).y(event.getRawY() + yCoordinate).setDuration(0).start();
+                        break;
+                }
+                return true;
+            }
+        });
     }
 }
